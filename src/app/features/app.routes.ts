7a0227f02a33aaaa03429d790de45323960c1b60
tounekti <dtounakty@gmail.com>
2025-08-25 20:31:19 +0100

    frs myacc inrf

--- src/app/features/app.routes.ts
+++ src/app/features/app.routes.ts
@@ -6,12 +6,14 @@ import { NotfoundComponent } from './../notfound/notfound.component';
 import { HomeComponent } from './../home/home.component';
 import { AuthComponent } from '../auth/auth.component';
 import { SignupComponent } from '../signup/signup.component';
+import { MyaccountComponent } from '../myaccount/myaccount.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route (replace with your home component if needed)
   { path: 'about', component: AboutComponent },
   { path: 'service', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'myaccount', component: MyaccountComponent },
   { path: '404', component: NotfoundComponent },
   { path: 'login', component: AuthComponent },
   { path: 'logup', component: SignupComponent },
--- src/app/myaccount/myaccount.component.ts
+++ src/app/myaccount/myaccount.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-myaccount',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './myaccount.component.html',
   styleUrl: './myaccount.component.css'
 })
